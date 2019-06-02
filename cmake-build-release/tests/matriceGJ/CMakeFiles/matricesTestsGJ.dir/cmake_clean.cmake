9cacb664f007a90cfb0c892f8a646f9c6a7a3536
Matt <simcrimsos@gmail.com>
2019-06-02 18:14:46 +0200

    added release profil

---
+++ cmake-build-release/tests/matriceGJ/CMakeFiles/matricesTestsGJ.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matricesTestsGJ.dir/testsMatriceGaussJordan.cpp.obj"
+  "matricesTestsGJ.pdb"
+  "matricesTestsGJ.exe"
+  "matricesTestsGJ.exe.manifest"
+  "matricesTestsGJ.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matricesTestsGJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
